537c2573679df46a2bf3ca4c54962502d6574fa3
retno2777 <144577662+retno2777@users.noreply.github.com>
2024-10-02 11:11:56 +0700

    Initial commit

---
+++ packages/frontend/src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import App from "./App.tsx";
+import "./index.css";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./store.ts";
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+	<StrictMode>
+		<Provider store={store}>
+				<App />
+		</Provider>
+	</StrictMode>,
+);
